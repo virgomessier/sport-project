7010e37ff125826384f008b26941042202c03a91
VIRGO <virgomessier104@protonmail.com>
2023-11-21 23:09:16 +0400

    Add script.js and add into default setting

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+/*jslint devel: true, browser: true, white: true*/
+/*jslint maxerr: 10, es6, node, single, for, bitwise, for, multivar*/
+/*eslint-env es6, es2017, es2018*/
+/*eslint-disable no-console,*/
+/*global console*/
+
+const isChecked = false,
+      isClose = true;
+
+console.log(isChecked && isClose);
