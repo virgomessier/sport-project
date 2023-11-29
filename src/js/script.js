c1d7d4b726d98434b7e97ede673a90da14948447
VIRGO <virgomessier104@protonmail.com>
2023-11-29 16:55:34 +0400

    add carousel and start make next section

--- src/js/script.js
+++ src/js/script.js
@@ -4,20 +4,38 @@
 /*eslint-disable no-console,*/
 /*global console*/
 
-$(document).ready(function(){
-  $('.carousel__inner').slick({
-    speed: 600,
-    adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"</button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"</button>',
-    responsive: [
-    {
-      breakpoint: 992,
-      settings: {
-          dots: true,
-          arrows: false,
-      }
-    },
-  ]
-    });
+//$(document).ready(function(){
+//  $('.carousel__inner').slick({
+//    speed: 600,
+//    adaptiveHeight: true,
+//    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"</button>',
+//    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"</button>',
+//    responsive: [
+//    {
+//      breakpoint: 992,
+//      settings: {
+//          dots: true,
+//          arrows: false,
+//      }
+//    },
+//  ]
+//    });
+//});
+
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+  });
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo("prev");
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo("next");
 });
