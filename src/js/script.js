a2b3e16afb58b4ea4c0165861b34aa4a9f109f85
VIRGO <virgomessier104@protonmail.com>
2023-12-01 03:55:18 +0400

    made all js for tabs

--- src/js/script.js
+++ src/js/script.js
@@ -4,24 +4,27 @@
 /*eslint-disable no-console,*/
 /*global console*/
 
-//$(document).ready(function(){
-//  $('.carousel__inner').slick({
-//    speed: 600,
-//    adaptiveHeight: true,
-//    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"</button>',
-//    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"</button>',
-//    responsive: [
-//    {
-//      breakpoint: 992,
-//      settings: {
-//          dots: true,
-//          arrows: false,
-//      }
-//    },
-//  ]
-//    });
-//});
+$(document).ready(function () {
 
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.items-wrapper').removeClass('items-wrapper_active').eq($(this).index()).addClass('items-wrapper_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.items-wrapper__content').eq(i).toggleClass('items-wrapper__content_unactive');
+                $('.items-wrapper__list').eq(i).toggleClass('items-wrapper__list_unactive');
+            })
+        });
+    }
+
+    toggleSlide('.items-wrapper__more');
+    toggleSlide('.items-wrapper__list-link');
+});
 
 const slider = tns({
     container: '.carousel__inner',
