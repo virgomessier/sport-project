b9d7b09464d0968b140ba586e8af97730d02e71f
VIRGO <virgomessier104@protonmail.com>
2023-12-09 03:13:32 +0400

    add gitignore and jq code

--- src/js/script.js
+++ src/js/script.js
@@ -70,7 +70,25 @@ $(document).ready(function () {
     valideForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return;
+        }
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
+
+
 
 const slider = tns({
     container: '.carousel__inner',
